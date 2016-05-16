ced6e968050fab73150a407ad3fd259befa759a6
Lee JongWon <jay09144@gmail.com>
2016-05-16 20:34:44 +0900

    calculator

--- calculator.cpp
+++ calculator.cpp
@@ -5,15 +5,20 @@ int main()
 {
 	int fnum=0;
 	int snum=0;
+	int result=0;
 	
 	cin >> fnum;
 	cin >> snum;
 
-	cout << fnum+snum << endl;
 
+	result = fnum+snum;
+	cout << result << endl;
 
-	cout << fnum-snum << endl;
+	result = fnum-snum;
+	cout << result << endl;
 
+	result = fnum/snum;
+	cout << result << endl;
 return 0;
 }
 
